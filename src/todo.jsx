90f05e98683672642fe8bd9037c525ccb8caf42e
siddhi-gargs <183721797+siddhi-gargs@users.noreply.github.com>
2025-05-04 17:18:11 +0530

    worst code

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,25 +1,12 @@
-import { useState, Component } from "react";
+import { Component } from "react";
 import "./todo.css";
-
-function Item(props) {
-  return (
-    <>
-      <input
-        type="checkbox"
-        checked={props.done}
-        onChange={() => props.toggleStatus(props.taskId, !props.done)}
-      />
-      <span>{props.task}</span>
-    </>
-  );
-}
 
 class Input extends Component {
   constructor(props) {
     super(props);
     this.state = { value: "" };
     this.change = this.change.bind(this);
-    this.keyPress = this.saveAndClear.bind(this);
+    this.saveAndClear = this.saveAndClear.bind(this);
   }
 
   change(event) {
@@ -41,63 +28,166 @@ class Input extends Component {
           type="text"
           value={this.state.value}
           onChange={this.change}
-          onKeyDown={this.keyPress}
+          onKeyDown={this.saveAndClear}
         />
       </>
     );
   }
 }
 
-function Todo({ taskId }) {
-  const allTask = [
-    { task: "buy clothes", done: true, taskId: 1 },
-    { task: "buy surf", done: true, taskId: 2 },
-  ];
-
-  const [items, setItems] = useState(allTask);
-
-  function toggle(taskId, done) {
-    setItems((prevItems) =>
-      prevItems.map((item) =>
-        item.taskId === taskId ? { ...item, done } : item
-      )
-    );
-  }
-
-  function addTask(task) {
-    setItems((prev) => [...prev, { task, done: false, taskId: taskId() }]);
-  }
-
+function Item(props) {
   return (
-    <p>
-      <Input onSubmit={addTask} />
-      {items.map((item) => (
-        <Item
-          task={item.task}
-          done={item.done}
-          taskId={item.taskId}
-          toggleStatus={toggle}
-          key={item.taskId}
-        />
-      ))}
-    </p>
+    <>
+      <input
+        type="checkbox"
+        checked={props.task.done}
+        onChange={() => props.toggleStatus(props.task.taskId, !props.task.done)}
+      />
+      <span>{props.task.task}</span>
+    </>
   );
 }
 
-class multipleTodos extends Component {
+class Todo extends Component {
   constructor(props) {
     super(props);
-    this.state = { todos: [] };
   }
 
-  // render() {
-  //   return (
-
-  //   )
-  // }
+  render() {
+    console.log(this.props);
+    return (
+      <p>
+        <div>{this.props.todo.title}</div>
+        <Input
+          onSubmit={(task) => this.props.addTask(this.props.todo.todoId, task)}
+        />
+        {this.props.tasks.map((task) => (
+          <Item
+            task={task}
+            toggleStatus={(taskId, done) =>
+              this.props.toggle(this.props.todo.todoId, taskId, done)
+            }
+            key={task.taskId}
+            todoId={this.props.todo.todoId}
+          />
+        ))}
+        {/* <Item
+          done={this.props.todo.done}
+          taskId={this.props.todo.taskId}
+          toggleStatus={this.props.toggle}
+          
+        /> */}
+      </p>
+    );
+  }
 }
 
-export default function App() {
+class MultipleTodos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          todoId: 1,
+          title: "siddhi",
+          tasks: [
+            {
+              taskId: 1,
+              task: "buy clothes",
+              done: false,
+            },
+            {
+              taskId: 2,
+              task: "buy clothes",
+              done: false,
+            },
+          ],
+        },
+        {
+          todoId: 2,
+          title: "sameera",
+          tasks: [
+            {
+              taskId: 1,
+              task: "buy clothes",
+              done: false,
+            },
+            {
+              taskId: 2,
+              task: "buy clothes",
+              done: false,
+            },
+          ],
+        },
+      ],
+    };
+
+    this.addTodo = this.addTodo.bind(this);
+    this.addTask = this.addTask.bind(this);
+    this.toggle = this.toggle.bind(this);
+  }
+
+  addTask(todoId, task) {
+    this.setState((prev) => ({
+      todos: prev.todos.map((todo) => {
+        if (todo.todoId === todoId) {
+          return {
+            ...todo,
+            tasks: [
+              ...todo.tasks,
+              { task, taskId: this.props.taskId(), done: false },
+            ],
+          };
+        }
+        return todo;
+      }),
+    }));
+  }
+
+  toggle(todoId, taskId, done) {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.todoId === todoId) {
+          return {
+            ...todo,
+            tasks: todo.tasks.map((task) =>
+              task.taskId === taskId ? { ...task, done } : task
+            ),
+          };
+        } else {
+          return todo;
+        }
+      }),
+    }));
+  }
+
+  addTodo(title) {
+    this.setState((prev) => ({
+      todos: [...prev.todos, { todoId: this.props.todoId(), title, tasks: [] }],
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Add New To-Do List</h2>
+        <Input onSubmit={this.addTodo} />
+        {this.state.todos.map((todo) => (
+          <Todo
+            todo={todo}
+            tasks={todo.tasks}
+            key={todo.taskId}
+            addTask={this.addTask}
+            toggle={this.toggle}
+            probs={this.props}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+function App() {
   function generateId() {
     let count = 5;
     return () => count++;
@@ -108,9 +198,9 @@ export default function App() {
 
   return (
     <div>
-      <Todo taskId={taskId} todo={todoId}>
-        {" "}
-      </Todo>
+      <MultipleTodos taskId={taskId} todoId={todoId} />
     </div>
   );
 }
+
+export default App;
